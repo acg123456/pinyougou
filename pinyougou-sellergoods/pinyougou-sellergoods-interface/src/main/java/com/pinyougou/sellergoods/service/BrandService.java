557a04ff73eabe438cf201b5526e34d958257e4b
acg <1326984242@qq.com>
2019-08-16 14:54:34 +0800

    8.16导入项目

---
+++ pinyougou-sellergoods/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -0,0 +1,28 @@
+package com.pinyougou.sellergoods.service;
+
+import com.github.pagehelper.PageInfo;
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.service.BaseService;
+
+import java.util.List;
+
+public interface BrandService extends BaseService<TbBrand> {
+    List<TbBrand> queryAll();
+
+    /**
+     * 根据分页参数分页查询品牌列表
+     * @param pageNum 页号
+     * @param pageSize 页大小
+     * @return 品牌列表
+     */
+    List<TbBrand> testPage(Integer pageNum, Integer pageSize);
+
+    /**
+     * 根据条件分页查询
+     * @param pageNum 页号
+     * @param pageSize 页大小
+     * @param brand 查询条件对象
+     * @return 分页信息对象
+     */
+    PageInfo<TbBrand> search(Integer pageNum, Integer pageSize, TbBrand brand);
+}
